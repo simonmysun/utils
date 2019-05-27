cb307b21e1c3dc6d53aa319f4c55abcde571b8e0
simonmysun <simonmysun@gmail.com>
2019-05-27 21:02:28 +0200

    update

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 sh ~/utils/swap_keys.sh
 sh ~/utils/multiscreen/150.sh
 sh ~/utils/calibrate_touchscreen/multiscreen.sh
+setsid 'emacs' --daemon
 sh ~/utils/rs.sh 
---
+++ time.js
@@ -0,0 +1,13 @@
+var stdTimezoneOffset = function (date) {
+    var jan = new Date(date.getFullYear(), 0, 1);
+    var jul = new Date(date.getFullYear(), 6, 1);
+    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+}
+var isDstObserved = function (date) {
+    return date.getTimezoneOffset() < stdTimezoneOffset(date);
+}
+// https://stackoverflow.com/questions/11887934/how-to-check-if-the-dst-daylight-saving-time-is-in-effect-and-if-it-is-whats
+
+var d = new Date();
+
+console.log(`${'So,Mo,Di,Mi,Do,Fr,Sa'.split(',')[d.getDay()]}. ${d.getDate()}. ${'Jan,Feb,Mrz,Apr,Mai,Jun,Jul,Aug,Sep,Okt,Nov,Dez'.split(',')[d.getMonth()]}. ${d.getFullYear()} ${d.toTimeString().slice(0, 8)} ${isDstObserved(d) ? 'CEST (UTC+2)' : 'CST (UTC+1)'}`)
