1acbe09c4fd255c91edbde8fd64a9ff356291022
simonmysun <simonmysun@gmail.com>
2022-04-04 05:48:40 +0200

    update

--- ln.sh
+++ ln.sh
@@ -1,7 +1,2 @@
 ln -s ~/git/simonmysun/praxis/utils/ ~/utils
-ln -s ~/utils/.bashrc ~/.bashrc
-ln -s ~/utils/.bashrcd ~/.bashrcd
-ln -s ~/utils/.vimrc ~/.vimrc
-ln -s ~/utils/.mintty ~/.mintty
-ln -s ~/utils/.Xmodmap ~/.Xmodmap
-ln -s ~/utils/.tmux.conf ~/.tmux.conf
+
