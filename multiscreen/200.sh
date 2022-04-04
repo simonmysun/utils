4d7c3abe202bfc18b92bf8013e31e0f83a5bab30
simonmysun <simonmysun@gmail.com>
2022-04-04 05:49:29 +0200

    update

--- multiscreen/200.sh
+++ multiscreen/200.sh
@@ -1,1 +1,1 @@
-xrandr --output eDP-1 --auto --output HDMI-2 --auto --panning 5120x2880+3840+0 --scale 2x2 --right-of eDP-1 --rotate normal
+xrandr --output eDP-1 --auto --output HDMI-2 --auto --panning 3840x2160+3840+0 --scale 2x2 --right-of eDP-1 --rotate normal
