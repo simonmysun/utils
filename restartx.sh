dd477d0d15e7d377bfd8c0e13db72e4fa1c3b07c
simonmysun <simonmysun@gmail.com>
2022-04-21 11:52:24 +0200

    prompts

---
+++ restartx.sh
@@ -0,0 +1,1 @@
+killall xfce4-session && ps aux | grep xfce4-session
--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ sh ~/utils/swap_keys.sh
 #sh ~/utils/multiscreen/150.sh
 #sh ~/utils/calibrate_touchscreen/multiscreen.sh
 setsid 'emacs' --daemon
-setsid antimicrox
+# setsid antimicrox
 # sh ~/utils/rs.sh &
 # sh ~/utils/forward_vnc_server.sh &
 wait
