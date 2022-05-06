1ace97f637857f765e7e167098cf93ff5db11d9f
simonmysun <simonmysun@gmail.com>
2022-05-06 23:04:31 +0200

    update

--- pashare.sh
+++ pashare.sh
@@ -9,9 +9,9 @@ case "$1" in
     # pactl list | grep "Monitor Source"
     ;;
   start)
-    sh $0 stop 
-    # pactl load-module module-simple-protocol-tcp rate=48000 format=s16le channels=2 source=alsa_output.pci-0000_00_1f.3.analog-stereo.monitor record=true port=8901
-    pactl load-module module-simple-protocol-tcp rate=48000 format=s16le channels=2 source=ladspa_output.mbeq_1197.mbeq.monitor record=true port=8901
+    sh $0 stop
+    pactl load-module module-simple-protocol-tcp rate=48000 format=s16le channels=2 source=alsa_output.pci-0000_00_1f.3.analog-stereo.monitor record=true port=8901
+    # pactl load-module module-simple-protocol-tcp rate=48000 format=s16le channels=2 source=ladspa_output.mbeq_1197.mbeq.monitor record=true port=8901
     ;;
   stop)
     pactl unload-module `pactl list | grep tcp -B1 | grep M | sed 's/[^0-9]//g'`
