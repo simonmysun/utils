5ea82f0caffd3e1beb41ba03404856fe694e9736
Maoyin Sun <simonmysun@gmail.com>
2024-02-08 17:35:17 +0100

    remove unused startup script

--- startup.sh
+++
@@ -1,8 +0,0 @@
-sh ~/utils/swap_keys.sh
-#sh ~/utils/multiscreen/150.sh
-#sh ~/utils/calibrate_touchscreen/multiscreen.sh
-setsid 'emacs' --daemon
-# setsid antimicrox
-# sh ~/utils/rs.sh &
-# sh ~/utils/forward_vnc_server.sh &
-wait
