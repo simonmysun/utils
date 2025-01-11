b2d30ccf3605769d6148814123137b3ba2559b24
Maoyin Sun <simonmysun@gmail.com>
2025-01-12 00:59:24 +0100

    use slower preset and lower crf

--- ffmpeg/reencode_pixel_video.sh
+++ ffmpeg/reencode_pixel_video.sh
@@ -3,21 +3,22 @@
 INPUT=$1
 INPUT_FILENAME="${INPUT%.*}"
 
+mkdir -p transcoded
+
 ffmpeg \
   -i "${INPUT}" \
-  -map 0 \
   -map_chapters 0 \
   -movflags use_metadata_tags \
   -map_metadata 0 \
   -ignore_unknown \
   -c:d copy \
-  -c:v libx265 \
-  -maxrate 200M \
-  -bufsize 1000M \
+  -c:v libsvtav1 \
+  -crf 20 \
+  -preset 5 \
   -c:a libopus \
   -b:a 128K \
-  "${INPUT_FILENAME}.reencoded.mp4" && \
+  "./transcoded/${INPUT_FILENAME}.reencoded.mp4" && \
   exiftool \
   -TagsFromFile "${INPUT}" \
   "-all:all>all:all" \
-  "${INPUT_FILENAME}.reencoded.mp4"
+  "./transcoded/${INPUT_FILENAME}.reencoded.mp4"
