4e9d4a85782d4e78643d1557812fd41902ca2711
Maoyin Sun <simonmysun@gmail.com>
2025-02-06 00:35:12 +0100

    loop until interrupt when changing dpms

--- dpms.sh
+++ dpms.sh
@@ -15,24 +15,33 @@ fi
 echo ">>>DPMS: $(xset -q | grep "DPMS is" | awk '{print $3}')"
 echo ">>>Screensaver timeout: $(xset -q | grep "timeout:" | awk '{print $2}')"
 
-if [ $1 == 'enable' ]; then
-    echo "<<<diable screensaver";
-    xset s off;
-    echo "<<<enable dpms";
-    xset +dpms;
-    echo "<<<force monitor off";
-    xset dpms force off;
-elif [ $1 == 'disable' ]; then
-    echo "<<<diable screensaver anyway";
-    xset s off;
-    echo "<<<force monitor on";
-    xset dpms force on;
-    echo "<<<disable dpms";
-    xset -dpms;
-else
-    echo '!!!Unknown command';
-    print_usage;
-fi
-
-echo ">>>DPMS: $(xset -q | grep "DPMS is" | awk '{print $3}')"
-echo ">>>Screensaver timeout: $(xset -q | grep "timeout:" | awk '{print $2}')"
+while true; do
+    if [ $1 == 'enable' ]; then
+        echo "<<<diable screensaver";
+        xset s off;
+        echo "<<<enable dpms";
+        xset +dpms;
+        echo "<<<force monitor off";
+        xset dpms force off;
+        echo "<<<set dpms timeout 30 45 60";
+        xset dpms 30 45 60;
+    elif [ $1 == 'disable' ]; then
+        echo "<<<diable screensaver anyway";
+        xset s off;
+        echo "<<<force monitor on";
+        xset dpms force on;
+        echo "<<<disable dpms";
+        xset -dpms;
+        echo "<<<set dpms timeout to 0 0 0";
+        xset dpms 0 0 0;
+    else
+        echo '!!!Unknown command';
+        print_usage;
+    fi
+    echo ">>>DPMS: $(xset -q | grep "DPMS is" | awk '{print $3}')"
+    echo ">>>Screensaver timeout: $(xset -q | grep "timeout:" | awk '{print $2}')"
+    echo "";
+    echo "---Press Ctrl+C to exit";
+    sleep 30;
+    echo "";
+done
