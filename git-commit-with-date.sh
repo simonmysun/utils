b998ae17ca3785fd6b6a0f7c11355f73530c5b04
Maoyin Sun <simonmysun@gmail.com>
2025-09-10 13:16:11 +0200

    pass parameters to git

--- git-commit-with-date.sh
+++ git-commit-with-date.sh
@@ -12,7 +12,8 @@ if [ $# -lt 1 ]; then
     print_usage;
 fi
 
-TARGET_DATE=$(date "${@}");
+TARGET_DATE=$(date -d "${1}");
+shift;
 export GIT_AUTHOR_DATE="${TARGET_DATE}"
 export GIT_COMMITTER_DATE="${TARGET_DATE}"
-git commit "--date=${TARGET_DATE}"
+git commit "--date=${TARGET_DATE}" "$@"
