a1ed847c0c049404b7ca4fac5c99a11d6740afaf
Maoyin Sun <simonmysun@gmail.com>
2026-07-06 07:44:16 +0200

    update

--- ln.sh
+++ ln.sh
@@ -1,4 +1,3 @@
 ln -sfn ~/git/simonmysun/utils/ ~/utils
 sudo ln -sfn ~/git/simonmysun/utils/etc.issue /etc/issue
 sudo ln -sfn ~/git/simonmysun/utils/etc.issue.net /etc/issue.net
-ln -sfn ~/git/simonmysun/utils/home-dot-config-systemd-user-emacs.service ~/.config/systemd/user/emacs.service
